d04b7c4801ef9b66520d4b65956d1872297b9c08
Jesse Brisola <jesse.araujo4@fatec.sp.gov.br>
2020-10-29 10:45:22 -0300

    layout finalizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { MdComputer } from "react-icons/md"
+
 import "./styles/styles.css"
+
+import storiesImg from './images/icons/circle.svg'
+import statusImg from './images/icons/status.svg'
+import moreImg from './images/icons/more.svg'
+import searchImg from './images/icons/search.svg'
+import checkImg from './images/icons/check.svg'
 
 function App() {
   return (
@@ -9,19 +17,84 @@ function App() {
       <div className="container">
         <div className="left">
           <div className="top">
-            <img src="https://avatars1.githubusercontent.com/u/28305012?s=460&u=e947608a2d0a560ea99595c3b37e3a02ef1ad93b&v=4" />
+            <img src="https://avatars1.githubusercontent.com/u/28305012?s=460&u=e947608a2d0a560ea99595c3b37e3a02ef1ad93b&v=4" alt="icone" />
             <div className="icons">
+              <img src={storiesImg} alt="icone" />
+              <img src={statusImg} alt="icone" />
+              <img src={moreImg} alt="icone" />
             </div>
           </div>
           <div className="search">
-            <input type="text" placeholder="Procurar ou começar uma conversa nova" />
+            <div className="searchGroup" >
+              <img src={searchImg} alt="icone" />
+              <input type="text" placeholder="Procurar ou começar uma conversa nova" />
+            </div>
           </div>
+
+          <div id="chatList" >
+            <div className="avatar">
+              <img src="https://engenharia360.com/wp-content/uploads/2019/05/esta-pessoa-nao-existe-engenharia360-4.png" alt="icone" />
+            </div>
+            <div className="label">
+              <div className="name_chat">
+                <h2>Marcos Andrade Souza</h2>
+                <div className="chatText">
+                  <p>Bom dia amigo</p>
+                </div>
+              </div>
+              <div className="hour">
+                12:00
+              </div>
+            </div>
+          </div>
+
+
+          <div id="chatList" >
+            <div className="avatar">
+              <img src="https://image.freepik.com/fotos-gratis/adulto-pele-oculos-pessoa-juventude_1187-6165.jpg" alt="icone" />
+            </div>
+            <div className="label">
+              <div className="name_chat">
+                <h2>Beatriz Maia</h2>
+                <div className="chatText">
+                  <img src={checkImg} alt="icone" /><p>Olha o projeto</p>
+                </div>
+              </div>
+              <div className="hour">
+                11:23
+              </div>
+            </div>
+          </div>
+
+
+          <div id="chatList" >
+            <div className="avatar">
+              <img src="https://img.ibxk.com.br/2019/02/17/17124052466014.jpg?w=328" alt="icone" />
+            </div>
+            <div className="label">
+              <div className="name_chat">
+                <h2>Roberto Carlos</h2>
+                <div className="chatText">
+                  <p>Cara precisa ver, ETs invadindo a terra agora</p>
+                </div>
+              </div>
+              <div className="hour">
+                Ontem
+              </div>
+            </div>
+          </div>
+
+
         </div>
+
+
+
+        {/**-------------------------------------------------------------------------------------------------------------------------------- */}
         <div className="right">
           <div className="contant">
 
 
-            <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" />
+            <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt="icone" />
             <h1>Mantenha seu celular conectado</h1>
 
             <p>
@@ -32,7 +105,7 @@ function App() {
             <hr />
 
             <p>
-              O WhatsApp está disponível para Windows. <a href="#">Obtenha-o aqui</a>
+              <MdComputer size={18} color="#919191" className="icon" alt="icone" />  O WhatsApp está disponível para Windows. <a href="https://www.whatsapp.com/download" target="_blank" alt=""> Obtenha-o aqui</a>.
             </p>
           </div>
         </div>
